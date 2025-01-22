0546b82e2badbc21f3ed22ff84d715d2805282cf
Tracey Kadenyi <treykadenyi@gmail.com>
2025-01-22 14:40:31 +0300

    Plus Minus challenge

--- HACKERANK QUESTIONS/plusMinus.js
+++ HACKERANK QUESTIONS/plusMinus.js
@@ -1,35 +1,20 @@
-// Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with  places after the decimal.
-
-// Note: This challenge introduces precision problems. The test cases are scaled to six decimal places, though answers with absolute error of up to  are acceptable.
-
-// Example
-// arr = [1, ,1 , 0, -1, -1]
-
-// There are  elements, two positive, two negative and one zero. Their ratios are ,  and . Results are printed as:
-
-// 0.400000
-// 0.400000
-// 0.200000
-
 const plusMinus = (arr) => {
-  let plus = [];
-  let minus = [];
-  let zero = [];
-  let len = arr.length;
+    let positives = 0, negatives = 0, zeroes = 0, n = arr.length;
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > 0) {
-      plus.push(arr[i]);
-    } else if (arr[i] < 0) {
-      minus.push(arr[i]);
-    } else {
-      zero.push(arr[i]);
+    for(let i = 0; i < n; i++) {
+        if(arr[i] > 0) {
+            positives++
+        } else if(arr[i] < 0) {
+            negatives++
+        } else {
+            zeroes++
+        }
     }
-  }
-  console.log((plus.length / len).toFixed(6));
-  console.log((minus.length / len).toFixed(6));
-  console.log((zero.length / len).toFixed(6));
-};
 
-arr = [1, 1, 0, -1, -1];
-console.log(plusMinus(arr));
+    console.log((positives/n).toFixed(6))
+    console.log((negatives/n).toFixed(6))
+    console.log((zeroes/n).toFixed(6))
+}
+
+arr = [1, 1, 0, -1, -1]
+console.log(plusMinus(arr))
