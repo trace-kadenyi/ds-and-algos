46c7c3c5116f56956abfa6a53775dbf55999b5a9
Tracey Kadenyi <treykadenyi@gmail.com>
2025-03-05 14:35:08 +0300

    Designer PDF Viewer challenge

---
+++ HACKERANK QUESTIONS/designerPdfViewer.js
@@ -0,0 +1,20 @@
+const designerPdfViewer = (h, word) => {
+  let alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+  let store = {};
+
+  // assign key/value pairs
+  for (const letter of alphabet) {
+    let index = alphabet.indexOf(letter);
+    store[letter] = h[index];
+  }
+  // find heights of the word
+  let heights = word.split("").map((w) => store[w]);
+
+  return Math.max(...heights) * heights.length;
+};
+
+(h = [
+  1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 1, 1, 5, 5, 1, 5, 2, 5, 5, 5, 5, 5, 5,
+]),
+  (word = "torn");
+console.log(designerPdfViewer(h, word));
