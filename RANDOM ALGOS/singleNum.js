1041ae42a2c9a38afd7d43e8f2bc8c5406946c6f
Tracey Kadenyi <treykadenyi@gmail.com>
2025-03-10 14:54:02 +0300

    Update singleNum.js

--- RANDOM ALGOS/singleNum.js
+++ RANDOM ALGOS/singleNum.js
@@ -1,8 +1,21 @@
+// Method 1
 const singleNum = (arr) => {
-    return arr.reduce((a, c) => a ^ c, 0);
-  };
-  
-  console.log(singleNum([2, 2, 1])); // Output: 1
-  console.log(singleNum([4, 1, 2, 1, 2])); // Output: 4
-  console.log(singleNum([7, 3, 5, 5, 3])); // Output: 7
-  
+  return arr.reduce((a, c) => a ^ c, 0);
+};
+
+// Method 2
+const singleNum1 = (arr) => {
+  let hash = {};
+
+  for (const num of arr) {
+    hash[num] = (hash[num] || 0) + 1;
+  }
+
+  for (const num in hash) {
+    if (hash[num] === 1) return num;
+  }
+};
+
+console.log(singleNum1([2, 2, 1])); // Output: 1
+console.log(singleNum1([4, 1, 2, 1, 2])); // Output: 4
+console.log(singleNum1([7, 3, 5, 5, 3])); // Output: 7
